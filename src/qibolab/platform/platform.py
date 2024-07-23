1781f22e786c5b08c293a5f78ffbde4d617e6d98
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2024-07-23 19:54:41 +0300

    fix: platform execute

--- src/qibolab/platform/platform.py
+++ src/qibolab/platform/platform.py
@@ -203,7 +203,17 @@ class Platform:
 
     def _execute(self, sequences, options, **kwargs):
         """Executes sequence on the controllers."""
-        return self.sweep(sequences[0], options)
+        result = {}
+
+        for instrument in self.instruments.values():
+            if isinstance(instrument, Controller):
+                new_result = instrument.play(
+                    options.component_configs, sequences, options, {}
+                )
+                if isinstance(new_result, dict):
+                    result.update(new_result)
+
+        return result
 
     def execute_pulse_sequence(
         self,
