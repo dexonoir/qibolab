9039af45bd29cc9d847324c4b6fe9bfc862bade0
Paul Tan <paultan_1@hotmail.com>
2024-08-08 14:19:36 +0800

    fix: Support for new results and channels API

--- src/qibolab/instruments/keysight_qcs.py
+++ src/qibolab/instruments/keysight_qcs.py
@@ -8,7 +8,6 @@ from qibolab.execution_parameters import (
     ExecutionParameters,
 )
 from qibolab.instruments.abstract import Controller
-from qibolab.instruments.port import Port
 from qibolab.pulses import (
     Drag,
     Envelope,
@@ -18,21 +17,7 @@ from qibolab.pulses import (
     PulseType,
     Rectangular,
 )
-from qibolab.qubits import QubitId
-from qibolab.result import (
-    AveragedIntegratedResults,
-    AveragedRawWaveformResults,
-    AveragedSampleResults,
-    IntegratedResults,
-    RawWaveformResults,
-    SampleResults,
-)
 from qibolab.sweeper import Parameter, Sweeper
-
-
-class QCSPort(Port):
-    output_channel: qcs.Channels
-    input_channel: qcs.Channels = None
 
 
 def generate_envelope_qcs(shape: Envelope):
@@ -50,18 +35,18 @@ def generate_envelope_qcs(shape: Envelope):
 class KeysightQCS(Controller):
     """Interaction for interacting with QCS main server."""
 
-    PortType = QCSPort
-
     def __init__(
         self,
         name,
         address,
         channel_mapper: qcs.ChannelMapper,
-        qubit_readout_channels: dict[QubitId, qcs.Channels],
+        drive_channel_map: dict[str, qcs.Channels],
+        acquire_channel_map: dict[str, qcs.Channels],
     ):
         super().__init__(name, address)
         self.mapper = channel_mapper
-        self.qubit_readout_map = qubit_readout_channels
+        self.drive_channel_map = drive_channel_map
+        self.acquire_channel_map = acquire_channel_map
 
     def connect(self):
         if not self.is_connected:
@@ -113,47 +98,52 @@ class KeysightQCS(Controller):
 
         # Map of virtual Z rotations to qubits for phase tracking
         vz_map = {}
+        acquisitions = {}
 
         # Iterate over the pulses in the sequence and add them to the program in order
-        for pulse in sequence:
-            envelope = generate_envelope_qcs(pulse.envelope)
+        for channel, pulses in sequence.items():
+            qcs_channel = self.drive_channel_map[channel]
+            for pulse in pulses:
+                envelope = generate_envelope_qcs(pulse.envelope)
 
-            if pulse.type is PulseType.FLUX or pulse.type is PulseType.COUPLERFLUX:
-                qcs_pulse = qcs.DCWaveform(
-                    duration=pulse.duration * 1e-9,
-                    envelope=envelope,
-                    amplitude=pulse.amplitude,
-                )
-            elif pulse.type is PulseType.DELAY:
-                qcs_pulse = qcs.Delay(pulse.duration * 1e-9)
-            elif pulse.type is PulseType.VIRTUALZ:
-                # While QCS supports a PhaseIncrement instruction, in our case,
-                # the phase is relative to the qubit and not the channel
-                vz_map[pulse.qubit] = vz_map.get(pulse.qubit, 0) + pulse.phase
-                continue
-            else:
-                qcs_pulse = qcs.RFWaveform(
-                    duration=pulse.duration * 1e-9,
-                    envelope=envelope,
-                    amplitude=pulse.amplitude,
-                    frequency=pulse.frequency,
-                    instantaneous_phase=pulse.relative_phase
-                    + vz_map.get(pulse.qubit, 0),
-                )
-                if pulse.type is PulseType.READOUT:
-                    program.add_acquisition(
-                        pulse.duration * 1e-9,
-                        self.ports(pulse.channel).input_channel,
+                if pulse.type is PulseType.FLUX or pulse.type is PulseType.COUPLERFLUX:
+                    qcs_pulse = qcs.DCWaveform(
+                        duration=pulse.duration * 1e-9,
+                        envelope=envelope,
+                        amplitude=pulse.amplitude,
                     )
+                elif pulse.type is PulseType.DELAY:
+                    qcs_pulse = qcs.Delay(pulse.duration * 1e-9)
+                elif pulse.type is PulseType.VIRTUALZ:
+                    # While QCS supports a PhaseIncrement instruction, in our case,
+                    # the phase is relative to the qubit and not the channel
+                    vz_map[pulse.qubit] = vz_map.get(pulse.qubit, 0) + pulse.phase
+                    continue
+                else:
+                    qcs_pulse = qcs.RFWaveform(
+                        duration=pulse.duration * 1e-9,
+                        envelope=envelope,
+                        amplitude=pulse.amplitude,
+                        frequency=pulse.frequency,
+                        instantaneous_phase=pulse.relative_phase
+                        + vz_map.get(pulse.qubit, 0),
+                    )
+                    if pulse.type is PulseType.READOUT:
+                        readout_channel = self.acquire_channel_map[channel]
+                        program.add_acquisition(
+                            pulse.duration * 1e-9,
+                            readout_channel,
+                        )
+                        acquisitions[pulse.id] = readout_channel
 
-                if isinstance(pulse.envelope, Drag):
-                    qcs_pulse = qcs_pulse.drag(pulse.shape.beta)
+                    if isinstance(pulse.envelope, Drag):
+                        qcs_pulse = qcs_pulse.drag(pulse.shape.beta)
 
-            # If this pulse is part of a sweeper, set the variable
-            if pulse in sweeper_pulse_map:
-                parameter, qcs_var = sweeper_pulse_map[pulse]
-                setattr(qcs_pulse, parameter, qcs_var)
-            program.add_waveform(qcs_pulse, self.ports(pulse.channel).output_channel)
+                # If this pulse is part of a sweeper, set the variable
+                if pulse in sweeper_pulse_map:
+                    parameter, qcs_var = sweeper_pulse_map[pulse]
+                    setattr(qcs_pulse, parameter, qcs_var)
+                program.add_waveform(qcs_pulse, qcs_channel)
 
         # Set the number of shots
         program.n_shots(options.nshots)
@@ -162,40 +152,27 @@ class KeysightQCS(Controller):
         # Run the program on the backend
         self.backend.apply(program)
 
-        qubits_to_measure = [pulse.qubit for pulse in sequence.ro_pulses]
         results = {}
-        for qubit in qubits_to_measure:
-            if options.acquisition_type is AcquisitionType.RAW:
-                res = program.get_trace(
-                    channels=self.qubit_readout_map[qubit],
-                    avg=options.averaging_mode is not AveragingMode.SINGLESHOT,
+        averaging_mode = options.averaging_mode != AveragingMode.SINGLESHOT
+        for readout_id, readout_channel in acquisitions.items():
+            res = (
+                program.get_trace(
+                    channels=readout_channel,
+                    avg=averaging_mode,
                 )
-                results[qubit] = (
-                    RawWaveformResults(res)
-                    if AveragingMode.SINGLESHOT
-                    else AveragedRawWaveformResults(res)
+                if options.acquisition_type is AcquisitionType.RAW
+                else (
+                    program.get_iq(
+                        channels=readout_channel,
+                        avg=averaging_mode,
+                    )
+                    if options.acquisition_type is AcquisitionType.INTEGRATION
+                    else program.get_classified(
+                        channels=readout_channel,
+                        avg=averaging_mode,
+                    )
                 )
-
-            elif options.acquisition_type is AcquisitionType.INTEGRATION:
-                res = program.get_iq(
-                    channels=self.qubit_readout_map[qubit],
-                    avg=options.averaging_mode is not AveragingMode.SINGLESHOT,
-                )
-                results[qubit] = (
-                    IntegratedResults(res)
-                    if AveragingMode.SINGLESHOT
-                    else AveragedIntegratedResults(res)
-                )
-
-            elif options.acquisition_type is AcquisitionType.DISCRIMINATION:
-                res = program.get_classified(
-                    channels=self.qubit_readout_map[qubit],
-                    avg=options.averaging_mode is not AveragingMode.SINGLESHOT,
-                )
-                results[qubit] = (
-                    SampleResults(res)
-                    if AveragingMode.SINGLESHOT
-                    else AveragedSampleResults(res)
-                )
+            )
+            results[readout_id] = res.to_numpy()
 
         return results
